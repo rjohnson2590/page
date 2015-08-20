2ed89fad13f5ebf9049a93a2408505d3701e2125
Richard Johnson <rjohnson2590@gmail.com>
2015-08-20 15:09:21 -0700

    Fixing things on site

--- game.js
+++ game.js
@@ -146,7 +146,7 @@ function update(){
   ctx.fillRect(player.x, player.y, player.width, player.height);
   if(player.x>=85 && player.x<=95 && player.y>=40 && player.y<=50){
     var newUrl=['game.html']
-    alert('Access Granted')
+    window.alert('Access Granted')
     window.location.replace(newUrl)
   }
 
